0b4c15bf0fabe09257e1955ca7644ba3955e04eb
Roni769361 <60136750+Roni769361@users.noreply.github.com>
2023-03-16 17:28:14 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const toggleButton = document.getElementById("toggleButton");
+const hiddenParagraph = document.querySelector(".hidden");
+
+toggleButton.addEventListener("click", () => {
+  if (hiddenParagraph.style.display === "none") {
+    hiddenParagraph.style.display = "block";
+    toggleButton.innerText = "Read Less";
+  } else {
+    hiddenParagraph.style.display = "none";
+    toggleButton.innerText = "Read More";
+  }
+});
+
+function toggleRegistration() {
+    var regSection = document.getElementById("reg");
+    if (regSection.style.display === "none") {
+        regSection.style.display = "block";
+    } else {
+        regSection.style.display = "none";
+    }
+}
+
+var button = document.getElementById("button");
+button.classList.add("animated");
